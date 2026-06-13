3dbd69b3d0908649d770100e8e59ff07f813ba76
VECTORniqa <justpassingby0807@gmail.com>
2026-06-13 15:53:09 +0530

    Added init and hash-object functions

--- git.py
+++ git.py
@@ -29,39 +29,39 @@ def hash_object():
     if len(sys.argv) != 3:
         sys.exit("Enter a file you want to hash")
 
-        if "/" in sys.argv[2]:
-            full_path = sys.argv[2]
-            if not os.path.isfile(full_path):
-                sys.exit("Specified path/file does not exist")
-            else: 
-                
-                file_name= Path(sys.argv[2])
-                
-        else:
-            current_directory = os.getcwd() 
-            file_path = f"{current_directory}/{sys.argv[2]}"
-            if not os.path.isfile(file_path):
-                sys.exit("Specified file does not exist")
+    if "/" in sys.argv[2]:
+        full_path = sys.argv[2]
+        if not os.path.isfile(full_path):
+            sys.exit("Specified path/file does not exist")
+        else: 
             
-            else:
-                file_name = sys.argv[2]
+            file_name= Path(sys.argv[2])
+            
+    else:
+        current_directory = os.getcwd() 
+        file_path = f"{current_directory}/{sys.argv[2]}"
+        if not os.path.isfile(file_path):
+            sys.exit("Specified file does not exist")
         
-        file_stats = os.stat(file_name)
-        file_size = file_stats.st_size
-        blob = f"blob {file_size}\0"
-        blob = blob.encode()
-        try:
-            with open(file_name, "rb") as file:
-                file_content = file.read()
-                blob += file_content
-        
-        except IOError:
-            sys.exit("File not found")
-        
-        h = blake2b()
-        h.update(blob)
-        blob_hash = h.hexdigest()
-        print(blob_hash)
+        else:
+            file_name = sys.argv[2]
+    
+    file_stats = os.stat(file_name)
+    file_size = file_stats.st_size
+    blob = f"blob {file_size}\0"
+    blob = blob.encode()
+    try:
+        with open(file_name, "rb") as file:
+            file_content = file.read()
+            blob += file_content
+    
+    except IOError:
+        sys.exit("File not found")
+    
+    h = blake2b()
+    h.update(blob)
+    blob_hash = h.hexdigest()
+    print(blob_hash)
         
 
 
