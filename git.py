26cf40c8891ec71bbaeda364f99e8bed13891f93
VECTORniqa <justpassingby0807@gmail.com>
2026-06-16 06:53:41 +0530

    Added init and hash-object function (now also stores hash object)

--- git.py
+++ git.py
@@ -2,6 +2,7 @@ import sys
 import os
 from pathlib import Path
 from hashlib import blake2b
+import zlib
     
 def check_sys():
     if len(sys.argv) < 2:
@@ -16,9 +17,9 @@ def init():
     
     #Creating git folders
     else:
-        create_folders(f"{full_path}/.git")
-        create_folders(f"{full_path}/.git/objects")
-        create_folders(f"{full_path}/.git/refs")
+        create_folders(os.path.join(full_path, ".git"))
+        create_folders(os.path.join(full_path, ".git", "objects"))
+        create_folders(os.path.join(full_path, ".git", "refs"))
         with open(f"{full_path}/.git/HEAD", "a") as file:
             pass
         
@@ -26,42 +27,95 @@ def init():
             pass
 
 def hash_object():
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         sys.exit("Enter a file you want to hash")
+    
+    
+    if len(sys.argv) == 3:
+        
+        file_name = Path(sys.argv[2])
 
-    if "/" in sys.argv[2]:
-        full_path = sys.argv[2]
-        if not os.path.isfile(full_path):
-            sys.exit("Specified path/file does not exist")
-        else: 
+        if file_name.is_file():
+
+            file_stats = os.stat(file_name)
+            file_size = file_stats.st_size
+            blob = f"blob {file_size}\0"
+            blob = blob.encode()
+            try:
+                with open(file_name, "rb") as file:
+                    file_content = file.read()
+                    blob += file_content
             
-            file_name= Path(sys.argv[2])
+            except IOError:
+                sys.exit("File not found")
             
-    else:
-        current_directory = os.getcwd() 
-        file_path = f"{current_directory}/{sys.argv[2]}"
-        if not os.path.isfile(file_path):
-            sys.exit("Specified file does not exist")
+            h = blake2b(digest_size=20)
+            h.update(blob)
+            blob_hash = h.hexdigest()
+            print(blob_hash)
         
         else:
-            file_name = sys.argv[2]
+            print("File Doesn't Exist")
     
-    file_stats = os.stat(file_name)
-    file_size = file_stats.st_size
-    blob = f"blob {file_size}\0"
-    blob = blob.encode()
-    try:
-        with open(file_name, "rb") as file:
-            file_content = file.read()
-            blob += file_content
-    
-    except IOError:
-        sys.exit("File not found")
-    
-    h = blake2b()
-    h.update(blob)
-    blob_hash = h.hexdigest()
-    print(blob_hash)
+    elif len(sys.argv) == 4 and sys.argv[3] == "-w":
+        file_name = Path(sys.argv[2])
+
+        if file_name.is_file():
+            
+            
+            
+            
+
+            file_stats = os.stat(file_name)
+            file_size = file_stats.st_size
+            blob = f"blob {file_size}\0"
+            blob = blob.encode()
+            try:
+                with open(file_name, "rb") as file:
+                    file_content = file.read()
+                    blob += file_content
+            
+            except IOError:
+                sys.exit("File not found")
+            
+            h = blake2b(digest_size=20)
+            h.update(blob)
+            blob_hash = h.hexdigest()
+            
+
+            current = Path.cwd()
+            current_length = str(current).split("\\")
+            
+
+            for i in range(len(current_length)):
+                git_objects = current / ".git" / "objects"
+                if git_objects.is_dir():
+                    break
+
+                current = current.parent
+            else:
+                sys.exit("Git not initialized")
+
+            hash_string = str(blob_hash)
+            folder1 = hash_string[:2]
+            file1 = hash_string[2:]
+            hash_file_path = os.path.join(git_objects, folder1, file1)
+            compressed_blob = zlib.compress(blob)
+            if not os.path.isdir(os.path.join(git_objects, folder1)):
+                create_folders(os.path.join(git_objects, folder1))
+            if not os.path.isfile(os.path.join(git_objects, folder1, file1)):
+                with open(hash_file_path, "wb") as file:
+                    file.write(compressed_blob)
+
+        else:
+            print("File Doesn't Exist")
+
+        
+        
+        
+        
+        
+
         
 
 
